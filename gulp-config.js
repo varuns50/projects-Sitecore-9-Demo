75c8a1f680534449f2be1acdcaab7070fbd9d9bd
vikas.suthar <vikas.suthar@adapty.com>
2018-12-19 13:11:21 +0530

    default sync settings

--- gulp-config.js
+++ gulp-config.js
@@ -1,19 +1,19 @@
 module.exports = function () {
-  var instanceRoot = "C:\\inetpub\\wwwroot\\habitat.dev.local";
-  var config = {
-    websiteRoot: instanceRoot + "\\",
-    sitecoreLibraries: instanceRoot + "\\bin",
-    licensePath: instanceRoot + "\\App_Data\\license.xml",
-    packageXmlBasePath: ".\\src\\Project\\Habitat\\code\\App_Data\\packages\\habitat.xml",
-    packagePath: instanceRoot + "\\App_Data\\packages",
-    solutionName: "Habitat",
-    buildConfiguration: "Debug",
-    buildToolsVersion: 15.0,
-    buildMaxCpuCount: 0,
-    buildVerbosity: "minimal",
-    buildPlatform: "Any CPU",
-    publishPlatform: "AnyCpu",
-    runCleanBuilds: false
-  };
-  return config;
+    var instanceRoot = "C:\\inetpub\\wwwroot\\sc901demo.local";
+    var config = {
+        websiteRoot: instanceRoot + "\\",
+        sitecoreLibraries: instanceRoot + "\\bin",
+        licensePath: instanceRoot + "\\App_Data\\license.xml",
+        packageXmlBasePath: ".\\src\\Project\\Habitat\\code\\App_Data\\packages\\habitat.xml",
+        packagePath: instanceRoot + "\\App_Data\\packages",
+        solutionName: "Habitat",
+        buildConfiguration: "Debug",
+        buildToolsVersion: 15.0,
+        buildMaxCpuCount: 0,
+        buildVerbosity: "minimal",
+        buildPlatform: "Any CPU",
+        publishPlatform: "AnyCpu",
+        runCleanBuilds: false
+    };
+    return config;
 }
